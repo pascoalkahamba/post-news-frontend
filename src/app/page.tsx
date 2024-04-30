c17f2f69ac88ec9a18fe189c8bd27a55c0268084
Pascoal Kahamba <pascoalkahamba25@gmail.com>
2024-04-30 16:54:57 +0100

    I've created the component LanguageToggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ModeToggle } from "@/components/modeToggle";
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { MouseEventHandler, useState } from "react";
 type HandleClickProps = MouseEventHandler<HTMLElement> | undefined;
